273c540cf87ef8a3b34df6e563ab4766bce0a8bb
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2025-12-29 00:26:54 +0300

    Update main.cpp
    Добавлен вывод по т/з

--- main.cpp
+++ main.cpp
@@ -11,21 +11,36 @@ int main()
 {
     Matrix<int, 0> matrix;
     
-    for (int i=0; i<9; i++)
+    // Заполнение матрицы.
+    for (int i=0; i<=9; i++)
     {
         matrix[i][i] = i;
-        matrix[i][8 - i] = 8-i;
+        matrix[i][9 - i] = 9-i;
     }
     
+    // Вывод требуемого фрагмента матрицы.
     for (int i=1; i<=8; i++)
     {
       for (int j=1; j<=8; j++)
       {
-          std::cout<<matrix[i][j]<< "\t";
+          std::cout<<matrix[i][j]<< " ";
       }
       std::cout<< std::endl;
     }
     
-    std::cout<<matrix.size()<<std::endl;
+    // Количество занятых ячеек.
+    std::cout<< "\nNumber of occupied cells: "<<matrix.size()<<"\n\n";
+
+    // Вывод занятых ячеек вместе со своими позициями.
+    for(auto c: matrix)
+    {
+        std::pair<int, int> key;
+        int x;
+        int y;
+        int v;
+        std::tie(key, v) = c; 
+        std::tie(x, y) = key; 
+        std::cout << "matrix["<<x<<"][" << y << "] = " << v << std::endl;
+    }
 
 }
