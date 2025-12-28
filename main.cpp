dede3b6c944e518d9128f3b7a337a2843f7f92fb
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2025-12-28 22:02:29 +0300

    Update main.cpp
    Выведен  фрагмент матрицы от \[1,1\] до \[8,8\].  и количество занятых ячеек.

--- main.cpp
+++ main.cpp
@@ -9,29 +9,23 @@
 
 int main()
 {
-
-    Matrix<int, -1> matrix;
-
-    assert(matrix.size() == 0);  // Все ячейки свободны
-
-    auto a = matrix[0][0];
-    assert(a == -1);
-    assert(matrix.size() == 0);
+    Matrix<int, 0> matrix;
     
-    matrix[100][100] = 314;
-    assert(matrix[100][100] == 314);
-    assert(matrix.size() == 1);
-    // выведется одна строка
-    // 100100314
-    for(auto c: matrix)
+    for (int i=0; i<9; i++)
     {
-        std::pair<int, int> key;
-        int x;
-        int y;
-        int v;
-        std::tie(key, v) = c; 
-        std::tie(x, y) = key; 
-        //std::tie(x, y, v) = c;
-        std::cout << x << y << v << std::endl;
+        matrix[i][i] = i;
+        matrix[i][8 - i] = 8-i;
     }
+    
+    for (int i=1; i<=8; i++)
+    {
+      for (int j=1; j<=8; j++)
+      {
+          std::cout<<matrix[i][j]<< "\t";
+      }
+      std::cout<< std::endl;
+    }
+    
+    std::cout<<matrix.size()<<std::endl;
+
 }
