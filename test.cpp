142b6b4f9648448d72a8715aa0f75e426939e7d5
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2025-12-28 23:41:32 +0300

    Update test.cpp
    Добавлены тесты.

--- test.cpp
+++ test.cpp
@@ -4,10 +4,28 @@
 
 TEST(MatrixTest, DirectTest) {
  
+    Matrix<int, 0> matrix;
+    
+    EXPECT_EQ(matrix.size(), 0);
+    EXPECT_EQ(matrix[0][0], 0) ;
+    EXPECT_EQ(matrix[100][100], 0);
+    
+    EXPECT_EQ(matrix.size(), 0);
+    
+    for (int i=0; i<=9; i++)
+    {
+        matrix[i][i] = i;
+        matrix[i][9 - i] = 9-i;
+    }
+        
+    EXPECT_EQ(matrix.size(), 18);
+    
+    EXPECT_EQ(matrix[4][5], 5);
+    EXPECT_EQ(matrix[9][9], 9);
+
 }
 
   
-
 
 
 
